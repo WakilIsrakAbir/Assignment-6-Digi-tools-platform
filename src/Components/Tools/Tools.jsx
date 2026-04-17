9c3891113f9e095f83612b1112c27ebb4cf2c97c
Wakil Israk Abir <wakilisrakabir@gmail.com>
2026-04-17 17:46:49 +0600

    make tool card

--- src/Components/Tools/Tools.jsx
+++ src/Components/Tools/Tools.jsx
@@ -19,14 +19,40 @@ const Tools = ({ toolPromise }) => {
         </button>
         <button className="btn border-none px-8 rounded-4xl">Cart(2)</button>
       </div>
-      <div className="">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         {tools.map((tool) => (
-          <div>
-            <div>
-              <img src={tool.icon} alt="" />
-            </div>
-            <div>
-              <h2> {tool.name} </h2>
+          <div className="relative shadow-xl/30 p-5 m-8 rounded-xl">
+            <div className="">
+              <div className="">
+                <img
+                  className="border border-indigo-100 p-3 rounded-full"
+                  src={tool.icon}
+                  alt=""
+                />
+              </div>
+              <div>
+                <h2 className="text-3xl font-bold py-3"> {tool.name} </h2>
+                <p> {tool.description} </p>
+              </div>
+              <div>
+                <h1 className="font-semibold py-4">
+                  <span className="text-3xl ">${tool.price}</span>/{" "}
+                  {tool.period}
+                </h1>
+              </div>
+              <div>
+                <ul className="list-disc pl-5 mb-5">
+                  {tool.features.map((feature, index) => (
+                    <li key={index}>{feature}</li>
+                  ))}
+                </ul>
+              </div>
+              <button className="btn w-full rounded-4xl bg-violet-600 text-white">
+                Buy Now
+              </button>
+              <p className="absolute top-10 right-10 py-2 px-6 rounded-4xl bg-purple-100 text-purple-800 text-lg font-semibold">
+                {tool.tag}
+              </p>
             </div>
           </div>
         ))}
